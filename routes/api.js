b332a4241b0225126c2ecbe0fbff9e9146087be7
Miguel Lozano <Miguellozano37578@gmail.com>
2021-07-28 20:31:20 -0700

    init commit

---
+++ routes/api.js
@@ -0,0 +1,54 @@
+
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } })
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/", (req, res) => {
+  Workout.aggregate([
+      {
+        $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+      },
+    ])
+    .sort({ date: -1 })
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+      {
+        $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+      },
+    ])
+    .sort({ date: -1 })
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+module.exports = router;
